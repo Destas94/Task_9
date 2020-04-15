516a3c8ca6ee9ecf16202c46f49d48a7e84e987e
Dmitry <dim4n94@yandex.ru>
2020-04-15 21:10:23 +0300

    Initial commit

---
+++ src/Task.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        int size = input.nextInt();
+        int array[] = new int[size];
+        System.out.println("Введите элемент массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt();
+        }
+        System.out.print ("Введённые элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = array[i]*2;
+            System.out.print (" " + array[i]); // Выводим на экран, полученный массив
+        }
+        System.out.println();
+    }
+}
